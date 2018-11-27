61b5226a852a87ad737910fa4b320e709473f270
marcio <marcio.hsdl@gmail.com>
2018-11-27 17:55:01 -0300

    [client-view] | refactor

--- src/app/components/client-list/client-list.component.ts
+++ src/app/components/client-list/client-list.component.ts
@@ -23,6 +23,25 @@ export class ClientListComponent implements OnInit {
   constructor(private dataPersistence: DataPersistenceService) { }
 
 
+  private hasFilter(client: Client, filter: string): boolean {
+    return (
+      client.name.toLowerCase().includes(this.replace(filter.toLowerCase())) ||
+      client.cpf.toString().toLowerCase().includes(filter.toString().toLowerCase()));
+  }
+
+
+  private replace (name: String) {
+
+    let _name = name.toLowerCase();
+    _name = _name.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a');
+    _name = _name.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e');
+    _name = _name.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i');
+    _name = _name.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o');
+    _name = _name.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u');
+    return _name;
+  }
+
+
   public loadData() {
     this.clientsBeforeFiltred = this.dataPersistence.read('client');
     this.clientsAfterFiltred  = this.clientsBeforeFiltred;
@@ -55,31 +74,12 @@ export class ClientListComponent implements OnInit {
   }
 
 
-  handleFilter() {
+  public handleFilter() {
     if (!this.search || this.search === '') {
       this.clientsAfterFiltred = this.clientsBeforeFiltred;
       return;
     }
     this.clientsAfterFiltred = this.clientsBeforeFiltred.filter(client => this.hasFilter(client, this.search));
-  }
-
-
-  hasFilter(client: Client, filter: string): boolean {
-    return (
-      client.name.toLowerCase().includes(this.replace(filter.toLowerCase())) ||
-      client.cpf.toString().toLowerCase().includes(filter.toString().toLowerCase()));
-  }
-
-
-  replace (name: String) {
-
-    let _name = name.toLowerCase();
-    _name = _name.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a');
-    _name = _name.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e');
-    _name = _name.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i');
-    _name = _name.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o');
-    _name = _name.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u');
-    return _name;
   }
 
 
